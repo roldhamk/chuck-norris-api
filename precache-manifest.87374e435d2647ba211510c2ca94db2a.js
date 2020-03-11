8a70b685077be4952e5ecb4bdc1e53988259a14b
Rachael Oldham-Knott <roldhamk@gmail.com>
2020-03-11 10:02:21 +0000

    Updates

---
+++ precache-manifest.87374e435d2647ba211510c2ca94db2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48cf2887325d584b974ff13b456c30ca",
+    "url": "/chuck-norris-api/index.html"
+  },
+  {
+    "revision": "6350ccc0bb3fd39575ce",
+    "url": "/chuck-norris-api/static/css/main.f334f79f.chunk.css"
+  },
+  {
+    "revision": "5dfe9d9768dd6864ec50",
+    "url": "/chuck-norris-api/static/js/2.78021818.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/chuck-norris-api/static/js/2.78021818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6350ccc0bb3fd39575ce",
+    "url": "/chuck-norris-api/static/js/main.027aa764.chunk.js"
+  },
+  {
+    "revision": "9767776bf59d3ea9355b",
+    "url": "/chuck-norris-api/static/js/runtime-main.06c2dfef.js"
+  }
+]);
