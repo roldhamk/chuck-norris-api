6748edb4c297b6677b69fb8f233f6bdaea28d225
Rachael Oldham-Knott <roldhamk@gmail.com>
2020-03-11 12:01:08 +0000

    added gitignore

---
+++ build/precache-manifest.f9baa5324528a63592f73a10da40d930.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27473bbc43f994f9ada0d2c4e8f0daf4",
+    "url": "/chuck-norris-api/index.html"
+  },
+  {
+    "revision": "a0ec1b1ba38ce05d02bd",
+    "url": "/chuck-norris-api/static/css/main.c8b4a678.chunk.css"
+  },
+  {
+    "revision": "5dfe9d9768dd6864ec50",
+    "url": "/chuck-norris-api/static/js/2.78021818.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/chuck-norris-api/static/js/2.78021818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0ec1b1ba38ce05d02bd",
+    "url": "/chuck-norris-api/static/js/main.9a800f73.chunk.js"
+  },
+  {
+    "revision": "9767776bf59d3ea9355b",
+    "url": "/chuck-norris-api/static/js/runtime-main.06c2dfef.js"
+  }
+]);
