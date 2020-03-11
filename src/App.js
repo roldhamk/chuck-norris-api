8f31623cf5032749edede20ebe587450759f653f
Rachael Oldham-Knott <roldhamk@gmail.com>
2020-03-11 09:52:20 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    value: []
+  };
+
+  componentDidMount() {
+    this.getNew();
+  }
+  getNew = e => {
+    fetch("http://api.icndb.com/jokes/random")
+      .then(res => res.json())
+      .then(json => this.setState({ value: [json] }));
+  };
+  render() {
+    const { value } = this.state;
+    const getJoke = value.map((value, index) => {
+      return <p key={index}>{value.value.joke}</p>;
+    });
+    return (
+      <div className="main">
+        {getJoke}
+        <button onClick={this.getNew}>New Joke</button>
+      </div>
+    );
+  }
 }
 
 export default App;
